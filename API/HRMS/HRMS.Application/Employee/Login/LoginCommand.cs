96bbef8fd33c43cca9c2f3bb0bdd7cee50837d7c
Prakash <73493925+kp6998@users.noreply.github.com>
2025-06-30 17:54:03 +0530

    Delete API directory

--- API/HRMS/HRMS.Application/Employee/Login/LoginCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-
-namespace HRMS.Application.Employee.Login;
-
-public sealed record LoginCommand() : IRequest<LoginResponse>
-{
-    public string strUserName { get; set; } = string.Empty;
-    public string strPassword { get; set; } = string.Empty;
-}
