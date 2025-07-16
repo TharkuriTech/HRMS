6075995d63e44ba7aa0f4747671f2629edccd011
kp6998 <kp06091998@gmail.com>
2025-07-16 23:25:50 +0530

    logo change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,50 +1,50 @@
-import React , { useState } from "react";
-import  Header  from "./Common/Header.jsx";
+import React, { useState } from "react";
+import Header from "./Common/Header.jsx";
 import SideBar from "./Common/SideBar.jsx";
 import RouteConfiguration from "./Routing/RouteConfiguration.jsx";
 import Login from "./Auth/Login.jsx";
 import { useSelector } from "react-redux";
 
 export default function App() {
- const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem("isLoggedIn") === "true");
-//   const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
+	const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem("isLoggedIn") === "true");
+	//   const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
 
 
-  // Callback for successful login
-  const handleLoginSuccess = () => {
-    setIsLoggedIn(true);
-  };
-  
-  return (
-    <>
-  <div class="inner-wrapper">
-			<div id="loader-wrapper">
-				<div class="loader">
-				  <div class="dot"></div>
-				  <div class="dot"></div>
-				  <div class="dot"></div>
-				  <div class="dot"></div>
-				  <div class="dot"></div>
-				</div>
-			</div>
-  {!isLoggedIn ? (
-          <Login onLoginSuccess={handleLoginSuccess} />
-        ) : (
-          <>
-			<Header />
-			
-			<div class="page-wrapper">
-				<div class="container-fluid">
-					<div class="row">
-			<SideBar />
-            <RouteConfiguration />
+	// Callback for successful login
+	const handleLoginSuccess = () => {
+		setIsLoggedIn(true);
+	};
+
+	return (
+		<>
+			<div class="inner-wrapper">
+				<div id="loader-wrapper">
+					<div class="loader">
+						<div class="dot"></div>
+						<div class="dot"></div>
+						<div class="dot"></div>
+						<div class="dot"></div>
+						<div class="dot"></div>
 					</div>
 				</div>
+				{!isLoggedIn ? (
+					<Login onLoginSuccess={handleLoginSuccess} />
+				) : (
+					<>
+						<Header />
+
+						<div class="page-wrapper">
+							<div class="container-fluid">
+								<div class="row">
+									<SideBar />
+									<RouteConfiguration />
+								</div>
+							</div>
+						</div>
+					</>
+				)}
 			</div>
-			</>
-        )}
-		</div>
-		<div class="sidebar-overlay" id="sidebar_overlay"></div>
-    </>
-  );
+			<div class="sidebar-overlay" id="sidebar_overlay"></div>
+		</>
+	);
 }
